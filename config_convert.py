91bdac2a2afdb117ad3c4dde21963b9a93170832
cute-omega <92797441+cute-omega@users.noreply.github.com>
2025-09-04 21:10:22 +0800

    添加 GitHub Actions 工作流以定期部署到 GitHub Pages，并更新 requirements.txt 中的 requests 依赖版本

--- config_convert.py
+++ config_convert.py
@@ -10,6 +10,7 @@ logging.basicConfig(
 # Domains that should not be proxied now
 ExcludedDomains = ["*.googlevideo.com"]
 
+
 def parse_sc_config(file):
     origin_config: list[list[list[str], str | None, str]] = load(file)
     new_config = {"server": {"intercepts": {}, "preSetIpList": {}}}
@@ -21,16 +22,15 @@ def parse_sc_config(file):
         elif sni == "":
             sni = "none"
 
-
         target: str = item[2]
         if target == "":
             target = "127.0.0.1"
         elif "[" in target:
-            continue # Skip IPv6 addresses
+            continue  # Skip IPv6 addresses
 
         domains: list[str] = item[0]
-        domain_rules=""
-        domain_num=len(domains)
+        domain_rules = ""
+        domain_num = len(domains)
         for key in domains:
 
             if key in ExcludedDomains:
@@ -50,7 +50,7 @@ def parse_sc_config(file):
                 domain_rules = domain
         if domain_num > 1:
             domain_rules = "(" + domain_rules + ")"
-            
+
         if key not in ExcludedDomains:
             new_config["server"]["intercepts"][domain_rules] = {}
             new_config["server"]["intercepts"][domain_rules][".*"] = {}
@@ -59,18 +59,15 @@ def parse_sc_config(file):
             if domain_rules not in new_config["server"]["preSetIpList"]:
                 new_config["server"]["preSetIpList"][domain_rules] = {}
             new_config["server"]["preSetIpList"][domain_rules][target] = True
-            
+
     return new_config
 
 
 def main():
     sc_fn = "sc-config.json"
-    sc_url = (
-        "https://github.com/SpaceTimee/Cealing-Host/raw/main/Cealing-Host.json".replace(
-            "github.com", "xget.xi-xu.me/gh"
-        )
-    )
+    sc_url = "https://github.com/SpaceTimee/Cealing-Host/raw/main/Cealing-Host.json"
     ds_fn = "ds-config.json"
+
     with open(sc_fn, "w") as file:
         file.write(get(sc_url, timeout=10).text)
 
