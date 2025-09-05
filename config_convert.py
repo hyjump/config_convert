8c30eef59f405daecdd9e318049857fe99c1dadd
cute-omega <92797441+cute-omega@users.noreply.github.com>
2025-09-05 15:27:30 +0800

    重构配置解析功能，添加预设和后置配置文件

--- config_convert.py
+++ config_convert.py
@@ -10,14 +10,24 @@ logging.basicConfig(
 # Domains that should not be proxied now
 ExcludedDomains = ["*.googlevideo.com"]
 
+sc_config_type = list[list[str], str | None, str]
+ds_config_type = dict[
+    str, dict[str, dict[str, dict[str, str | None]] | dict[str, dict[str, bool]]]
+]
 
-def parse_sc_config(file):
-    origin_config: list[list[list[str], str | None, str]] = load(file)
-    new_config = {"server": {"intercepts": {}, "preSetIpList": {}}}
-    for item in origin_config:
 
+def parse_sc_config(preset_fn: str, sc_fn: str, postset_fn: str) -> ds_config_type:
+    with open(preset_fn) as preset_file:
+        ds_config: ds_config_type = load(preset_file)
+    logging.info(f"Loaded preset config from {abspath(preset_fn)}")
+
+    with open(sc_fn) as sc_file:
+        sc_config: sc_config_type = load(sc_file)
+    logging.info(f"Loaded Sheas Cealer config from {abspath(sc_fn)}")
+
+    for item in sc_config:
         sni: str | None = item[1]
-        if sni == None:
+        if sni is None:
             continue
         elif sni == "":
             sni = "none"
@@ -25,45 +35,34 @@ def parse_sc_config(file):
         target: str = item[2]
         if target == "":
             target = "127.0.0.1"
-        elif "[" in target:
+        elif target.find("[") == -1:
             continue  # Skip IPv6 addresses
 
-        domains: list[str] = item[0]
-        domain_rules = ""
-        domain_num = len(domains)
-        for key in domains:
+        raw_domains: list[str] = item[0]
+        domains = [
+            domain := raw_domain.lstrip("$#")
+            for raw_domain in raw_domains
+            if raw_domain.find("^") == -1 and (domain not in ExcludedDomains)
+        ]
+        domain_rules = "|".join(domains)
+        if len(raw_domains) > 1:
+            domain_rules = f"({domain_rules})"
 
-            if key in ExcludedDomains:
-                continue
+        if domain_rules:
+            ds_config["server"]["intercepts"][domain_rules] = {}
+            ds_config["server"]["intercepts"][domain_rules][".*"] = {}
+            ds_config["server"]["intercepts"][domain_rules][".*"]["sni"] = sni
 
-            domain = key.removeprefix("#").removeprefix("$").removeprefix("#")
-            # According to the documentation, domains starting with ^ are invalid
-            if domain.startswith("^"):
-                continue
-            domain = domain.removeprefix("#").removeprefix("$").removeprefix("#")
+            if domain_rules not in ds_config["server"]["preSetIpList"]:
+                ds_config["server"]["preSetIpList"][domain_rules] = {}
+            ds_config["server"]["preSetIpList"][domain_rules][target] = True
 
-            if domains.index(key) < domain_num - 1 and domain_num > 1:
-                domain_rules = domain_rules + domain + "|"
-            elif domains.index(key) == domain_num - 1 and domain_num > 1:
-                domain_rules = domain_rules + domain
-            else:
-                domain_rules = domain
-        if domain_num > 1:
-            domain_rules = "(" + domain_rules + ")"
-
-        if key not in ExcludedDomains:
-            new_config["server"]["intercepts"][domain_rules] = {}
-            new_config["server"]["intercepts"][domain_rules][".*"] = {}
-            new_config["server"]["intercepts"][domain_rules][".*"]["sni"] = sni
-
-            if domain_rules not in new_config["server"]["preSetIpList"]:
-                new_config["server"]["preSetIpList"][domain_rules] = {}
-            new_config["server"]["preSetIpList"][domain_rules][target] = True
-
-    return new_config
+    return ds_config
 
 
 def main():
+    preset_fn = "ds-preset.json"
+    postset_fn = "ds-postset.json"
     sc_fn = "sc-config.json"
     sc_url = "https://github.com/SpaceTimee/Cealing-Host/raw/main/Cealing-Host.json"
     ds_fn = "ds-config.json"
@@ -72,10 +71,10 @@ def main():
         file.write(get(sc_url, timeout=10).text)
 
     logging.info(f"Downloaded latest Sheas Cealer config as {abspath(sc_fn)}")
-    with open(sc_fn, "r") as sc:
-        with open(ds_fn, "w") as ds:
-            ds_config = parse_sc_config(sc)
-            dump(ds_config, ds, ensure_ascii=False, indent=2)
+
+    ds_config = parse_sc_config(preset_fn, sc_fn, postset_fn)
+    with open(ds_fn, "w") as ds_file:
+        dump(ds_config, ds_file, ensure_ascii=False, indent=2)
     logging.info(f"Converted to Dev-Sidecar config and saved as {abspath(ds_fn)}")
 
 
